826560d9426298b906cc5ef5aa34a4f71c920761
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-03-30 12:57:12 -0300

    function attackMonster

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,8 +9,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-const attackHandler = () => {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+const attackMonster = (mode) => {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -20,6 +28,15 @@ const attackHandler = () => {
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('you have a draw')
   }
+}
+
+const attackHandler = () => {
+  attackMonster('ATTACK');
+};
+
+const strongAttackHandler = () => {
+  attackMonster('STRONG_ATTACK');
 };
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
